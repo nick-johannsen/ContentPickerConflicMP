2508c158c91029c8362a9de98c42fe2429761658
nick-johannsen <nick.johannsen@usafa.org>
2025-03-26 13:40:03 -0600

    Add project files.

---
+++ ContentPickerConflicMP/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "ContentPickerConflictTheme",
+    Author = "",
+    Website = "",
+    Version = "0.0.1",
+    Description = "For showing congflict for razor extension method"
+)]
